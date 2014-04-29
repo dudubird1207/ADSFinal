0c99a2dd9148f33cb240a4f278176491e3651cef
Greg Werbin <gw2286@columbia.edu>
2014-04-28 21:31:35 -0400

    Big changes made.
    All of the preliminary data processing is *done*, up to but not including the text mining.
    
    TODO.txt has the next step, and feel free to add more to the list.
    
    I highly recommend *not* running the scripts! Just call `load("jobs.RData")` to load up the data set.

---
+++ main.R
@@ -0,0 +1,11 @@
+# In case something gets messed up and you need to re-run a script.
+# This is the ONLY safe order for starting over.
+
+# source("data_processing.R")
+# creates "jobs.Rda"
+
+# source("exploratory.R")
+# (non-essential)
+
+# source("split_train_test.R")
+# creates "jobs.RData"
